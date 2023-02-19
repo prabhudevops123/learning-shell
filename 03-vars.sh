e793e45db685aeaa7fe5ad4908eec53feffac476
Prabhu <prabh.rao@gmail.com>
2023-02-19 22:26:27 +0530

    in out log and function

--- 03-vars.sh
+++ 03-vars.sh
@@ -4,7 +4,6 @@ echo "Hello, Good Morning, Today Date is ${DATE}"
 # Command Substitution
 DATE=$(date)
 echo "Hello, Good Morning, Today Date is ${DATE}"
-
 # Arithmetic Substitution
 ADD=$((2+3))
 echo "Addition of 2+3 = ${ADD}"
